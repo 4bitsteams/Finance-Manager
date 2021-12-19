a3ea5e57acb004ec708c3720bd77e6a39c9b9a9c
Maxim Mahanko <tolkoo.keeper@gmail.com>
2021-12-19 05:04:04 +0300

    Syncfusion licence registration moved to MainWindow constructor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ namespace FinanceManager
     {
         public MainWindow()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTQ3ODQ1QDMxMzkyZTMzMmUzMGRuaXFBbWx3cVNqajVCQnY0SmJic2dZMVJuWk1yNG9Vb1RUeDI4b0VLSWM9");
             InitializeComponent();
         }
 
--- Views/CategoryEditView.xaml.cs
+++ Views/CategoryEditView.xaml.cs
@@ -10,7 +10,6 @@ namespace FinanceManager.Views
     {
         public CategoryEditView()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTQ3ODQ1QDMxMzkyZTMzMmUzMGRuaXFBbWx3cVNqajVCQnY0SmJic2dZMVJuWk1yNG9Vb1RUeDI4b0VLSWM9");
             InitializeComponent();
         }
     }
